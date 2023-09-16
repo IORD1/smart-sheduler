db5cbd82ccab10c412f79563bc12a03e36805742
IORD1 <pratham111ingole@gmail.com>
2023-09-17 00:33:49 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <div className="container">
+        sadf
+        {/* AIzaSyA-6Om9C4ynVH8PhB_H7y8Pz5nbZYX80f4 */}
+        <pre id="content" style={{ whiteSpace: 'pre-wrap' }}></pre>
+    </div>
+  );
+}
+
+export default Home;
